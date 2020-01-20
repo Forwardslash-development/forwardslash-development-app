5984b3f7d4085aec46519a20a587868d3d807797
forwardslash development <jv.email@gmail.com>
2020-01-20 12:36:00 -0600

    repo cleanup

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"forwardslash-development-app","short_name":"forwardslash","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/img/42xlogo.png"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
