daa102ca020f42e0abd516ab77d158608b19098b
forwardslash development <jv.email@gmail.com>
2020-01-20 13:46:12 -0600

    added inputs to form

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Layout from '../components/layout';
+import Header from '../components/Header/header.jsx'
+import Footer from '../components/Footer/footer.jsx'
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+//import GridContainer from "../../../components/Grid/GridContainer.jsx";
+//import GridItem from "../../../components/Grid/GridItem.jsx";
+//import CustomInput from "../../../components/CustomInput/CustomInput.jsx";
+// import Button from "../../../components/CustomButtons/Button.jsx";
+
+import contactStyle from "assets/jss/material-kit-react/views/home/contactStyle.jsx";
 
 import bg6 from '../assets/img/bg6.jpeg';
 
@@ -12,11 +26,19 @@ const Success = props => (
     </Helmet>
 
     <div id="main" className="alt">
+      <Header
+        brand="Forwardslash Development"
+        // rightLinks={<HeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+      />
       <section id="one">
         <div className="inner">
-          <header className="major">
-            <h1>Success/Thank You Page</h1>
-          </header>
+          <h1>Success/Thank You Page</h1>
           <span className="image main">
             <img src={bg6} alt="" />
           </span>
@@ -24,7 +46,8 @@ const Success = props => (
         </div>
       </section>
     </div>
-  </Layout>
+    <Footer />
+  </Layout >
 );
 
 export default Success;
