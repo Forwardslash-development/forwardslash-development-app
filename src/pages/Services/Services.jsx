5e26bbebbb15403220b52a987c497aa80107363f
Forwardslash-development <jv@forwardslash-development.com>
2020-05-26 15:54:40 -0500

    description updated

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from "@material-ui/core/styles/withStyles"
 
 // @material-ui/icons
-import ThumbUp from "@material-ui/icons/ThumbUp";
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
+import ThumbUp from "@material-ui/icons/ThumbUp"
+import VerifiedUser from "@material-ui/icons/VerifiedUser"
+import Fingerprint from "@material-ui/icons/Fingerprint"
 // core components
 import GridContainer from "../../components/Grid/GridContainer.jsx"
-import GridItem from "../../components/Grid/GridItem.jsx";
-import InfoArea from "../../components/InfoArea/InfoArea.jsx";
+import GridItem from "../../components/Grid/GridItem.jsx"
+import InfoArea from "../../components/InfoArea/InfoArea.jsx"
 
-import servicesStyle from "assets/jss/material-kit-react/views/home/servicesStyle.jsx";
+import servicesStyle from "assets/jss/material-kit-react/views/home/servicesStyle.jsx"
 
 export class Services extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes } = this.props
     return (
       <div className={classes.section}>
         <GridContainer justify="center">
           <GridItem xs={12} sm={12} md={8}>
             <h2 className={classes.title}>Forwardslash Development Services</h2>
             <h5 className={classes.description}>
-              Although there are distinct languages such as query language that the Document Object Model DOM is a given context in the loads of universal isomorphic when the same machine. Rhino engine is an interpreted language, supporting object-oriented, imperative, and response objects, which an API for Node. Mediator Pattern is the script accordingly. BEM is Google’s library to modify page. NoSQL database.
+              Forwardslash Development provides progressive web application and
+              API development services. We create web-based solutions,
+              components, and API services to be used across various platforms
+              and devices.
             </h5>
           </GridItem>
         </GridContainer>
@@ -59,8 +62,8 @@ export class Services extends Component {
           </GridContainer>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default withStyles(servicesStyle)(Services);
+export default withStyles(servicesStyle)(Services)
